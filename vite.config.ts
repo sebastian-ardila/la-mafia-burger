257c81deb9bb57dcf6603a4521354b502d35d7ad
Sebastian Ardila <sardila@runmyprocess.com>
2026-04-05 19:24:47 -0500

    fix: correct base URL to match repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/la-mafia-burguer/',
+  base: '/la-mafia-burger/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
